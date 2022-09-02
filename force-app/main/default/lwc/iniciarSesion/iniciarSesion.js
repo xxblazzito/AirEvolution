4af837d8ddf97f9f16113863be9cc6654d4043dd
xxblazzito <cristiancamilo.aguirrerestrepo@gmail.com>
2022-09-01 19:30:48 -0500

    completando el inicio de sesion

--- force-app/main/default/lwc/iniciarSesion/iniciarSesion.js
+++ force-app/main/default/lwc/iniciarSesion/iniciarSesion.js
@@ -1,5 +1,7 @@
 import { LightningElement } from 'lwc';
 import iniciarSesion from '@salesforce/apex/GestionSesion.iniciarSesion';
+import nuevaSesion from '@salesforce/apex/GestionSesion.crearSesion';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 
 export default class IniciarSesion extends LightningElement {
     sesion;
@@ -7,6 +9,13 @@ export default class IniciarSesion extends LightningElement {
     password;
     datos;
     error;
+    creandoSesion=false;
+    usuarioC;
+    correoC;
+    password1C;
+    password2C;
+    datosNuevaSesion;
+    errorNuevaSesion;
 
     handleChange(event) {
         /* Una declaración de cambio que se usa para determinar qué input se está usando. */
@@ -19,6 +28,18 @@ export default class IniciarSesion extends LightningElement {
                  this.password= event.detail.value;
                  console.log("numero-->"+this.password);
                  break;
+            case 'nickC':
+                this.usuarioC = event.detail.value;
+                break;
+            case 'mailC':
+                this.correoC = event.detail.value;
+                break;
+            case 'pass1C':
+                this.password1C = event.detail.value;
+                break;
+            case 'pass2C':
+                this.password2C = event.detail.value;
+                break;
          }
     }
 
@@ -31,6 +52,7 @@ export default class IniciarSesion extends LightningElement {
                 console.log(resultado);
                 if(resultado != null){
                     this.sesion = true;
+                    this.notificacion('Bienvenid@: '+resultado.Name);
                 }
             }).catch((errores) => {
                 this.error = errores;
@@ -40,11 +62,52 @@ export default class IniciarSesion extends LightningElement {
             })
     }
 
+    crear(){
+        nuevaSesion({nick: this.usuarioC, pass: this.password1C, pass2: this.password2C, correo: this.correoC})
+            .then((result)=> {
+                this.datosNuevaSesion = result;
+                if(result != null){
+                    this.creandoSesion = false;
+                    console.log('sesion creada con exito');
+                    this.notificacion('La cuenta se ha creado exitosamente','bueno');
+                }else{
+                    console.log('Tu contraseña no coincide');
+                    this.notificacionMala('La contraseña no coincide, reintentelo nuevamente');
+                }
+            }).catch((error)=> {
+                this.errorNuevaSesion = error;
+                console.log('Ha ocurrido un error al crear la nueva cuenta');
+                this.notificacionMala('Error al crear la cuenta, si el error persiste comuniquese con soporte tecnico');
+            })
+    }
+
     restorePass(){
         console.log('Recuperando la contraseña');
     }
 
     createSesion(){
         console.log('Creanding cuenta');
+        this.creandoSesion = true;
     }
+
+    notificacion(mensaje){
+        const event = new ShowToastEvent({
+            title: 'Accion Exitosa',
+            message:
+                mensaje,
+            variant: 'success',
+        });
+        this.dispatchEvent(event);
+    }
+
+    notificacionMala(mensaje){
+        const event = new ShowToastEvent({
+            title: 'Error',
+            message:
+                mensaje,
+            variant: 'error',
+        });
+        this.dispatchEvent(event);
+    }
+    
 }
